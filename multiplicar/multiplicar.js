05d70399f26c4a4a85f25199ac5918ac327f6bd7
ricardo.loro <ricardo.loro.next@bbva.com>
2021-01-26 18:18:53 +0100

    Initial commit

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,39 @@
+const fs = require('fs'); //file system
+const colors = require('colors');
+
+
+let crearArchivo = (base, limite = 10) => {
+  return new Promise((resolve, reject) => {
+
+    if (!Number(base)) {
+      reject(`El valor introducido ${base} no es un número`);
+      return;
+    }
+
+    let data = ''
+
+    for (let i = 1; i <= limite; i++) {
+      data += `${base} * ${i} = ${base * i}\n`;
+    }
+    fs.writeFile(`./tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
+      if (err) reject(err);
+      else resolve(`tabla-${base}-al-${limite}.txt`);
+    });
+  })
+}
+
+let listarTabla = (base, limite = 10) => {
+  console.log('======================'.green);
+  console.log(`===  Tabla del ${base}  ====`.green);
+  console.log('======================'.green);
+  for (let i = 1; i <= limite; i++) {
+    console.log(`${base} * ${i} = ${base * i}`);
+  }
+}
+
+module.exports = {
+  crearArchivo,
+  listarTabla
+}
+
+
